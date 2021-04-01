abd56a7627bc77bf30d50e6c18a6ef88e18ecc38
Ramez <ramezzormati1998@gmail.com>
2021-04-01 02:04:42 +0100

    change 'conuslterStockPr' from get to post

--- src/main/java/com/orbit/portailAgentStockPR/consulterStockPr/controller/StockPrController.java
+++ src/main/java/com/orbit/portailAgentStockPR/consulterStockPr/controller/StockPrController.java
@@ -1,15 +1,10 @@
 package com.orbit.portailAgentStockPR.consulterStockPr.controller;
 
 import com.orbit.portailAgentStockPR.consulterStockPr.models.ListeStockAgentRequest;
-import com.orbit.portailAgentStockPR.consulterStockPr.models.ListeStockAgentResponse;
 import com.orbit.portailAgentStockPR.consulterStockPr.service.StockPrService;
-import com.orbit.portailAgentStockPR.utilisateur.models.AuthenticationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/ListeStockAgent")
@@ -18,7 +13,7 @@ public class StockPrController {
     @Autowired
     StockPrService stockPrService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<?> listeStockAgentWS(@RequestBody ListeStockAgentRequest req)
     {
         return ResponseEntity.ok(stockPrService.getArt(req));

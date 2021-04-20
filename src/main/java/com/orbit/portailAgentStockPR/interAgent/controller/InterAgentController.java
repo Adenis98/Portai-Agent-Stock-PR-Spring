239f17a92e41105ee0744a93dab7c79614a59866
Ramez <ramezzormati1998@gmail.com>
2021-04-20 05:08:58 +0100

    inter agent update cors

--- src/main/java/com/orbit/portailAgentStockPR/interAgent/controller/InterAgentController.java
+++ src/main/java/com/orbit/portailAgentStockPR/interAgent/controller/InterAgentController.java
@@ -3,12 +3,14 @@ package com.orbit.portailAgentStockPR.interAgent.controller;
 import com.orbit.portailAgentStockPR.interAgent.models.GetAllAgentsStockResponse;
 import com.orbit.portailAgentStockPR.interAgent.service.GetAllAgentsStockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class InterAgentController {
 
